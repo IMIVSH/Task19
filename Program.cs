d9cb9af829ea4c743c95903562a538a50c6a9631
IMIVSH <iamivanshumilin@gmail.com>
2022-12-29 19:44:10 +0300

    Task19 done with supposed function

--- Program.cs
+++ Program.cs
@@ -30,3 +30,27 @@ else
         Console.WriteLine("Число не палиндром.");
     }
 }
+
+
+/*
+Я думаю примерно вот так.
+Если отдельной задачей выводить.
+
+Console.Clear();
+
+Console.WriteLine("Введите ваше пятизначное число.");
+int inputNumber = int.Parse(Console.ReadLine()!);
+
+int findReverse (int inputNumber, int c, int reverse)
+{
+    while (c < 5)
+    {
+    reverse = reverse*10;
+    reverse = reverse + inputNumber%10;
+    inputNumber = inputNumber/10;
+    c++;
+    }
+    return reverse;
+}
+Console.WriteLine($"Ваше число перевёрнуто {findReverse(inputNumber, c, reverse)}.);
+*/
